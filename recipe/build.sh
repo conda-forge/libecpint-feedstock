099b143a1b5b38bed298dffa3e9fad1dbeb43612
Lori A. Burns <lori.burns@gmail.com>
2023-06-04 16:57:40 -0400

    iter 10 cross-compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,27 +1,38 @@
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+    ARCH_ARGS="-Dgenecpint_DIR=${PWD}/build_native"
+else
+    ARCH_ARGS=""
+fi
 
-echo PREFIX
-find ${PREFIX}/bin -name "*python*"
-echo BUILD_PREFIX
-find ${BUILD_PREFIX}/bin -name "*python*"
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+    cmake \
+      -S ${SRC_DIR} \
+      -B build_native \
+      -G Ninja \
+      -D CMAKE_C_COMPILER=${CC_FOR_BUILD} \
+      -D CMAKE_CXX_COMPILER=${CXX_FOR_BUILD}
 
+    cmake --build build_native --target generate
+fi
 
-cmake ${CMAKE_ARGS} \
+cmake ${CMAKE_ARGS} ${ARCH_ARGS} \
   -S ${SRC_DIR} \
   -B build \
   -G Ninja \
   -D CMAKE_INSTALL_PREFIX=${PREFIX} \
   -D CMAKE_BUILD_TYPE=Release \
+  -D CMAKE_C_COMPILER=${CC} \
+  -D CMAKE_CXX_COMPILER=${CXX}
   -D CMAKE_INSTALL_LIBDIR=lib \
   -D BUILD_SHARED_LIBS=ON \
   -D LIBECPINT_USE_PUGIXML=ON \
   -D LIBECPINT_BUILD_TESTS=ON \
   -D LIBECPINT_BUILD_DOCS=OFF \
-  -D Python_EXECUTABLE="${PREFIX}/bin/python" \
+  -D Python_EXECUTABLE="${BUILD_PREFIX}/bin/python" \
   -D CMAKE_PREFIX_PATH="${PREFIX}"
 
 cmake --build build --target install -j${CPU_COUNT}
 
-#  -D Python_EXECUTABLE="${BUILD_PREFIX}/bin/python"
 
 cd build
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
